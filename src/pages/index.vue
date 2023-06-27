875799111d94019521be07f9561ab43ab70ee613
Alston <p.truesword@gmail.com>
2023-06-28 00:13:17 +0800

    add no-select

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -20,21 +20,18 @@ const isTop1 = (item: Item) => getRank(item) === 1
 
 <template lang="pug">
 .flex.justify-center.bg-color
-  div.w-200
+  .w-200
     .flex.text-3xl.justify-center.my-2.font-bold SCOREBOARD
-
     div(v-for='item in items')
       .flex.border-b-1px.score-board-bg.text-white
         .flex.flex-grow.items-center.pl-4.text-2xl
           span {{ item.name }}
           span.ml-2(v-if='showRank(item) & item.score > 0') （第 {{ getRank(item) }} 名）
-          span(v-if='isTop1(item) & item.score > 0')
-            .i-icon-park-twotone-crown-three.text-yellow
-
+          .i-icon-park-twotone-crown-three.text-yellow(v-if='isTop1(item) & item.score > 0')
         .flex.text-3xl
-          .button.p-4.bg-red(@click='item.score--') -
+          .button.noselect.p-4.bg-red(@click='item.score--') -
           .flex.w-16.justify-center.items-center {{  item.score }}
-          .button.p-4.bg-green(@click='item.score++') +
+          .button.noselect.p-4.bg-green(@click='item.score++') +
 </template>
 
 <style lang="scss">
@@ -46,5 +43,14 @@ const isTop1 = (item: Item) => getRank(item) === 1
 }
 .button {
   cursor: pointer;
+}
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
 }
 </style>
